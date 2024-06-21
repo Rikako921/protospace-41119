ac0a059fb304d4f6c9882db3b883adcf90b6b3bc
Rikako921 <rico.kyg@icloud.com>
2024-06-22 00:13:07 +0900

    投稿機能を実装

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,11 +1,23 @@
 class PrototypesController < ApplicationController
   def index
+    
   end
 
   def new
+    @prototype = Prototype.new
   end
   
   def create
+    @prototype = Prototype.new(prototype_params)
+    if @prototype.save
+        redirect_to  '/'
+    else
+      render :new
   end
-  
+end
+private
+
+def prototype_params
+  params.require(:prototype).permit(:image, :title, :concept, :catch_copy).merge(user_id: current_user.id)
+end
 end
