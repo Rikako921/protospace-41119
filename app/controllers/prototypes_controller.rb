0c7ffc8598398cd05af52dc1c20e7b13a15e5b52
Rikako921 <rico.kyg@icloud.com>
2024-06-25 15:48:43 +0900

    コメントの投稿者名表示

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -21,7 +21,7 @@ end
 def show
   @prototype = Prototype.find(params[:id])
   @comment = Comment.new
-  @comments = @prototype.comments
+  @comments = @prototype.comments.includes(:user)
 end
 
 def edit
